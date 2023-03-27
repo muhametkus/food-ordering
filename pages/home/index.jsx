60e09cdbe04d4ba6f7b4e74fb950abecfcb49880
Muhammet Kus <77937107+muhametkus@users.noreply.github.com>
2023-03-28 00:07:06 +0300

    proje oluşturuldu ve font import edildi

---
+++ pages/home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <>
+    <div>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&family=Open+Sans&display=swap" rel="stylesheet" />
+</div>
+
+    <div className='text-red-600 font-dancing text-center text-3xl'>index</div></>
+  )
+}
+
+export default index
