48a4934519c6dc793dab3034cb0ee6173a1fd048
Muhammet Kus <77937107+muhametkus@users.noreply.github.com>
2023-03-28 06:41:35 +0300

    Header kısmı yapıldı. logo, menu, iconlar ve buton eklendi.

---
+++ pages/components/ui/Logo.jsx
@@ -0,0 +1,10 @@
+
+function Logo() {
+  return (
+    <div className="text-[2rem] font-dancing text-bold">
+        Feane
+    </div>
+  )
+}
+
+export default Logo
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
+import Header from '../components/layout/Header'
 
 const index = () => {
   return (
-    <>
+    
     <div>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&family=Open+Sans&display=swap" rel="stylesheet" />
+ <Header/>
 </div>
 
-    <div className='text-red-600 font-dancing text-center text-3xl'>index</div></>
+  
   )
 }
 
