a7a0573324e06d2ea56db70341e5e073597df1ad
Muhammet Kus <77937107+muhametkus@users.noreply.github.com>
2023-03-29 23:33:46 +0300

    Search component oluşturuldu.

--- pages/components/layout/Header.jsx
+++ pages/components/layout/Header.jsx
@@ -1,8 +1,18 @@
 import Logo from "../ui/Logo";
 import {FaUserAlt,FaShoppingCart,FaSearch} from "react-icons/fa";
+import { useState } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
+import Title from "../ui/Title";
+import { Search } from "../ui/Search";
 
 function Header() {
+
+  const[isSearchModal,setIsSearchModal]=useState(false)
+
+
+
   return (
+    <div>
     <div className="h-[5.5rem] bg-secondary">
         <div className="container mx-auto text-white  flex justify-between items-center h-full">
         <div> <Logo/></div>
@@ -26,18 +36,16 @@ function Header() {
 
 
 <div className="flex gap-x-4 items-center">
-    <a href=""><FaUserAlt/></a>
-    <a href=""><FaShoppingCart/></a>
-    <a href=""><FaSearch/></a>
+    <a href=""><FaUserAlt className="hover:text-primary transition-all"/></a>
+    <a href=""><FaShoppingCart className="hover:text-primary transition-all"/></a>
+    <button onClick={()=>setIsSearchModal(true)}><FaSearch className="hover:text-primary transition-all"/></button>
     <a href=""><button className="btn-primary">Order Online</button></a>
 </div>
-
-
-
-
-
+</div>
         </div>
+        {isSearchModal && <Search setIsSearchModal={setIsSearchModal}/>}
     </div>
+    
   )
 }
 
